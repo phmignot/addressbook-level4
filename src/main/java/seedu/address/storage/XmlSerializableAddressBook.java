b2ad6d4bbfc039d4db00e1a745df73be699f4e52
DESKTOP-7ICV2HR\ongkc <ongkc95@gmail.com>
2018-03-09 10:57:07 +0800

    Update team names

--- src/main/java/seedu/address/storage/XmlSerializableAddressBook.java
+++ src/main/java/seedu/address/storage/XmlSerializableAddressBook.java
@@ -3,10 +3,8 @@ package seedu.address.storage;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
-
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.AddressBook;
 import seedu.address.model.ReadOnlyAddressBook;
