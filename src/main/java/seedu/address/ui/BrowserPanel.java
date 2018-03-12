297b846e55c1b84ee7d54c47780c0433154e4767
phmignot <e0271089@u.nus.edu>
2018-03-12 21:33:58 +0800

    BrowserPanel back to original

--- src/main/java/seedu/address/ui/BrowserPanel.java
+++ src/main/java/seedu/address/ui/BrowserPanel.java
@@ -6,17 +6,12 @@ import java.util.logging.Logger;
 import com.google.common.eventbus.Subscribe;
 
 import javafx.application.Platform;
-import javafx.collections.ObservableList;
 import javafx.event.Event;
 import javafx.fxml.FXML;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
 import javafx.scene.layout.Region;
 import javafx.scene.web.WebView;
-import org.fxmisc.easybind.EasyBind;
 import seedu.address.MainApp;
 import seedu.address.commons.core.LogsCenter;
-import seedu.address.commons.events.ui.JumpToListRequestEvent;
 import seedu.address.commons.events.ui.PersonPanelSelectionChangedEvent;
 import seedu.address.model.person.Person;
 
@@ -25,76 +20,27 @@ import seedu.address.model.person.Person;
  */
 public class BrowserPanel extends UiPart<Region> {
 
+    public static final String DEFAULT_PAGE = "default.html";
+    public static final String SEARCH_PAGE_URL =
+            "https://se-edu.github.io/addressbook-level4/DummySearchPage.html?name=";
+
     private static final String FXML = "BrowserPanel.fxml";
 
-    private final Logger logger = LogsCenter.getLogger(PersonListPanel.class);
-    //private final Logger logger = LogsCenter.getLogger(this.getClass());
-
+    private final Logger logger = LogsCenter.getLogger(this.getClass());
 
     @FXML
-    private ListView<PersonCard> personListView;
+    private WebView browser;
 
-
-    public BrowserPanel(ObservableList<Person> personList) {
+    public BrowserPanel() {
         super(FXML);
-        setConnections(personList);
+
+        // To prevent triggering events for typing inside the loaded Web page.
+        getRoot().setOnKeyPressed(Event::consume);
+
+        loadDefaultPage();
         registerAsAnEventHandler(this);
     }
 
-    private void setConnections(ObservableList<Person> personList) {
-        ObservableList<PersonCard> mappedList = EasyBind.map(
-            personList, (person) -> new PersonCard(person, personList.indexOf(person) + 1));
-        personListView.setItems(mappedList);
-        personListView.setCellFactory(listView -> new PersonListViewCell());
-        setEventHandlerForSelectionChangeEvent();
-    }
-
-    private void setEventHandlerForSelectionChangeEvent() {
-        personListView.getSelectionModel().selectedItemProperty()
-            .addListener((observable, oldValue, newValue) -> {
-                if (newValue != null) {
-                    logger.fine("Selection in person list panel changed to : '" + newValue + "'");
-                    raise(new PersonPanelSelectionChangedEvent(newValue));
-                }
-            });
-
-    }
-
-    /**
-     * Scrolls to the {@code PersonCard} at the {@code index} and selects it.
-     */
-    private void scrollTo(int index) {
-        Platform.runLater(() -> {
-            personListView.scrollTo(index);
-            personListView.getSelectionModel().clearAndSelect(index);
-        });
-    }
-
-    @Subscribe
-    private void handleJumpToListRequestEvent(JumpToListRequestEvent event) {
-        logger.info(LogsCenter.getEventHandlingLogMessage(event));
-        scrollTo(event.targetIndex);
-    }
-
-    /**
-     * Custom {@code ListCell} that displays the graphics of a {@code PersonCard}.
-     */
-    class PersonListViewCell extends ListCell<PersonCard> {
-
-        @Override
-        protected void updateItem(PersonCard person, boolean empty) {
-            super.updateItem(person, empty);
-
-            if (empty || person == null) {
-                setGraphic(null);
-                setText(null);
-            } else {
-                setGraphic(person.getRoot());
-            }
-        }
-    }
-    
-/* former function used by BrowserPanel
     private void loadPersonPage(Person person) {
         loadPage(SEARCH_PAGE_URL + person.getName().fullName);
     }
@@ -102,29 +48,25 @@ public class BrowserPanel extends UiPart<Region> {
     public void loadPage(String url) {
         Platform.runLater(() -> browser.getEngine().load(url));
     }
-*/
+
     /**
      * Loads a default HTML file with a background that matches the general theme.
      */
-    /* Used by former BrowserPanel
     private void loadDefaultPage() {
         URL defaultPage = MainApp.class.getResource(FXML_FILE_FOLDER + DEFAULT_PAGE);
         loadPage(defaultPage.toExternalForm());
     }
-*/
+
     /**
      * Frees resources allocated to the browser.
      */
-    /* Used by former BrowserPanel
     public void freeResources() {
         browser = null;
     }
-*/
-    //@Subscribe
-    /* Former handleEvent 
+
+    @Subscribe
     private void handlePersonPanelSelectionChangedEvent(PersonPanelSelectionChangedEvent event) {
         logger.info(LogsCenter.getEventHandlingLogMessage(event));
         loadPersonPage(event.getNewSelection().person);
     }
-    */
 }
