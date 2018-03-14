3c0d22cfd8dae4b2f7ad6cdca00f7a2a779ddba1
DESKTOP-7ICV2HR\ongkc <ongkc95@gmail.com>
2018-03-14 08:48:02 +0800

    no message

--- src/main/java/seedu/address/ui/TransactionListPanel.java
+++ src/main/java/seedu/address/ui/TransactionListPanel.java
@@ -2,18 +2,16 @@ package seedu.address.ui;
 
 import java.util.logging.Logger;
 
-import com.google.common.eventbus.Subscribe;
-import javafx.application.Platform;
 import org.fxmisc.easybind.EasyBind;
 
+import com.google.common.eventbus.Subscribe;
+
 import javafx.collections.ObservableList;
-import javafx.fxml.FXML;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.Region;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.events.ui.JumpToListRequestEvent;
-import seedu.address.commons.events.ui.PersonPanelSelectionChangedEvent;
 import seedu.address.model.transaction.Transaction;
 
 
