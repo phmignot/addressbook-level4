27d80e8ba454c3222442e5782f09a2328a1ffcd2
Steven-Jia <stevenjia@outlook.com>
2018-04-05 00:21:57 +0800

    Ensure that transaction cannot be added if a payee is the payer

---
+++ src/main/java/seedu/address/model/transaction/exceptions/PayeeIsPayerException.java
@@ -0,0 +1,8 @@
+//@@author steven-jia
+package seedu.address.model.transaction.exceptions;
+
+/**
+ * Signals that one of the specified payees is the payer.
+ */
+public class PayeeIsPayerException extends Exception {
+}
