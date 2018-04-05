d8ef0c20d6d9b73b7ed2d8d209eb74a76096e9ba
Steven-Jia <stevenjia@outlook.com>
2018-04-05 17:40:14 +0800

    Delete unused exception

--- src/main/java/seedu/address/model/transaction/exceptions/PayeeIsPayerException.java
+++
@@ -1,8 +0,0 @@
-//@@author steven-jia
-package seedu.address.model.transaction.exceptions;
-
-/**
- * Signals that one of the specified payees is the payer.
- */
-public class PayeeIsPayerException extends Exception {
-}
