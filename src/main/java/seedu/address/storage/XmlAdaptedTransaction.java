f8b899be38cf7491d7eb21542475836ac66d65be
DESKTOP-7ICV2HR\ongkc <ongkc95@gmail.com>
2018-04-15 00:02:44 +0800

    Updated developer guide manual testing

--- src/main/java/seedu/address/storage/XmlAdaptedTransaction.java
+++ src/main/java/seedu/address/storage/XmlAdaptedTransaction.java
@@ -30,7 +30,7 @@ public class XmlAdaptedTransaction {
 
     public static final String MISSING_FIELD_MESSAGE_FORMAT = "Transaction's %s field is missing!";
     public static final String MISSING_FIELD_MESSAGE_FORMAT_DATE =
-            "Transaction's %s field is missing, or is in wrong format!"
+            "Transaction's %s field is missing, or is in wrong format"
                     + "(date format example: 2018-04-14T17:22:56.218+08:00";
 
     @XmlElement(required = true)
@@ -188,9 +188,10 @@ public class XmlAdaptedTransaction {
 
         final Date dateTime = this.dateTime;
 
-        if (this.payees == null) {
+        if (this.payees.isEmpty()) {
             throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, "Payees"));
         }
+
         for (XmlAdaptedPerson payee: this.payees) {
             validatePersonFields(payee.toModelType());
         }
