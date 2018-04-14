828fd75e75cf9701b6535c59c398d78812d7b028
DESKTOP-7ICV2HR\ongkc <ongkc95@gmail.com>
2018-04-14 20:19:28 +0800

    update date xml

--- src/main/java/seedu/address/storage/XmlAdaptedTransaction.java
+++ src/main/java/seedu/address/storage/XmlAdaptedTransaction.java
@@ -29,6 +29,9 @@ import seedu.address.model.transaction.TransactionType;
 public class XmlAdaptedTransaction {
 
     public static final String MISSING_FIELD_MESSAGE_FORMAT = "Transaction's %s field is missing!";
+    public static final String MISSING_FIELD_MESSAGE_FORMAT_DATE =
+            "Transaction's %s field is missing, or is in wrong format!"
+                    + "(date format example: 2018-04-14T17:22:56.218+08:00";
 
     @XmlElement(required = true)
     private String transactionType;
@@ -180,7 +183,7 @@ public class XmlAdaptedTransaction {
         //@@author steven-jia
         if (this.dateTime == null) {
             throw new IllegalValueException(String.format
-                    (MISSING_FIELD_MESSAGE_FORMAT, Date.class.getSimpleName()));
+                    (MISSING_FIELD_MESSAGE_FORMAT_DATE, Date.class.getSimpleName()));
         }
 
         final Date dateTime = this.dateTime;
